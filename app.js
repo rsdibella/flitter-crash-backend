f9555c8e1bfc60a4df8cbf4fa08f55c9d7473e78
Sabrina Rizzo <sabri.rizzo@gmail.com>
2023-02-02 18:42:39 +0100

    iniciando mongoose

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 
 const feedRoutes = require("./routes/feed");
 
+require("./lib/connectMongoose");
+
 const app = express();
 
 app.use(bodyParser.json());
